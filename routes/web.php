9044df84827b164ddfbe3d9574af075af7274848
cemaladl <190103279@stu.sdu.edu.kz>
2021-03-04 13:05:06 +0300

    files added

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('main');
+})->name('main');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
