ba2edda3d1c158230f5c5278f43884fc88240242
cemaladl <190103279@stu.sdu.edu.kz>
2021-03-25 17:45:12 +0300

    Controller Route Fixed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Client;
+
+use App\Http\Controllers\ClientController;
+
+
 
 
 Route::get('/', function () {
@@ -14,3 +21,13 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
+
+Route::get('client/add', function() {
+    DB::table('clients')->insert([
+        'name' => 'Cemal',
+        'surname' => 'BALTA',
+        'age' => 20
+    ]);
+});
+
+Route::get('client', [ClientController::class, 'index']);  
